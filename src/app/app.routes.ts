8d16d591cc0f1d49cd680fdb2a8cf033f9e7932d
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-10-30 11:59:09 +0100

    update routing and dashboard structure; replace dashboard component with router outlet and add landing page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,20 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddTaskComponent } from './dashboard/add-task/add-task.component';
+import { BoardComponent } from './dashboard/board/board.component';
+import { ContactsComponent } from './dashboard/contacts/contacts.component';
+import { SummaryComponent } from './dashboard/summary/summary.component';
 
 export const routes: Routes = [
-  
+  {path: '', component: LandingPageComponent},
+  {path: 'dashboard', component: DashboardComponent,
+    children: [
+        {path: 'add-task', component: AddTaskComponent},
+        {path: 'board', component: BoardComponent},
+        {path: 'contacts', component: ContactsComponent},
+        {path: 'summary', component: SummaryComponent},
+    ]
+  }
+
 ];
