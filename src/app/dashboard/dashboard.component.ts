2816ef7f59f8dccb5e94ade40b6db901b37a87c9
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-10-30 12:17:57 +0100

    refactor dashboard layout; add navbar and side navbar components

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
+import { SideNavbarComponent } from "../../shared/side-navbar/side-navbar.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, SideNavbarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
