8558978766c669940407208a99599e9766f53346
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-10-30 14:02:52 +0100

    refactor(add-task): move AddTaskComponent to shared directory and update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AddTaskComponent } from './dashboard/add-task/add-task.component';
+import { AddTaskComponent } from '../shared/add-task/add-task.component';
 import { BoardComponent } from './dashboard/board/board.component';
 import { ContactsComponent } from './dashboard/contacts/contacts.component';
 import { SummaryComponent } from './dashboard/summary/summary.component';
