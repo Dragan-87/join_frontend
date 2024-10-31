8d676acf00f9820af0cc04139c533a9123899cef
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-10-31 12:45:39 +0100

    feat(landing-page): implement login form with validation; enhance layout and styles

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    rememberMe: new FormControl(''),
+  });
 
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+
+  login() {
+    console.log('login');
+  }
 }
