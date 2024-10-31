c53e758921be656c27465735296f1ee16204a197
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-10-31 17:14:09 +0100

    feat(landing-page): enhance layout and styles; add LandingPageComponent, improve SCSS for responsiveness, and update side navbar transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashboardComponent],
+  imports: [RouterOutlet, DashboardComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
