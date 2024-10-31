d8ccd1c95f54509a710e2852ea9980095193bcd7
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-10-31 21:05:53 +0100

    feat(dashboard): implement communication service for summary display; update layout and styles in dashboard and summary components

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { CommunicationService } from './../../shared/service/communication/communication.service';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import { SideNavbarComponent } from "../../shared/side-navbar/side-navbar.component";
+import { NgClass } from '@angular/common';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, SideNavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, SideNavbarComponent, NgClass],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  communicationService = inject(CommunicationService)
 
 }
